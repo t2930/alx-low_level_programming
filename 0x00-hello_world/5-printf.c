abd21c50092f5b30d4c75918050cea6e0bcb351f
Tonny Kimanzi <33518957+t2930@users.noreply.github.com>
2022-09-08 09:14:36 -0700

    Preprocessor

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print a string
+ *  uses printf
+ * Returns to  0
+ */
+
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
