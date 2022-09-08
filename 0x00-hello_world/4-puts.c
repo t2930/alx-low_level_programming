1bdfbc082e992b24ceb5b9b99c94525f83b7a483
Tonny Kimanzi <33518957+t2930@users.noreply.github.com>
2022-09-08 10:21:30 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main: Entry point
- * Description: Prints Programming is like building a multilingual puzzle
- * return: 0
+ *Main -  Entry point
+ *Description: Prints Programming is like building a multilingual puzzle
+ *Return: 0
  */
-int main(void)
+int main(void);
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
