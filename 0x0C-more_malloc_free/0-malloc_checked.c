f22ddb1400a950e31de93fa79a02e1edf5479dd0
Tonny Kimanzi <33518957+t2930@users.noreply.github.com>
2022-10-06 10:31:27 -0700

    oxoc more

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*malloc_checked - allocate memory.
+*@b: size of the memory to be allocated.
+*Return: pointer;
+**/
+
+void *malloc_checked(unsigned int b)
+{
+char *p;
+
+p = malloc(b);
+if (p == NULL)
+{
+exit(98);
+return (NULL);
+}
+return (p);
+}
